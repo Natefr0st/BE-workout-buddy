964d18aa2766f92d12a066f0b09dbe08ef917680
Kevin <kevin_zino@yahoo.com>
2022-09-04 16:50:28 +0300

    WIP: new User model, controllers, routes

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require("../models/userModel");
+
+// Login user
+const loginUser = async (req, res) => {
+    res.json({msg: "login user"})
+}
+
+// Signup user
+const signupUser = async (req, res) => {
+    res.json({msg: "Signup user"})
+}
+
+module.exports = {signupUser, loginUser};
