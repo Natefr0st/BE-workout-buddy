cdf0307640d02d82d2431ea9026caff80f0254e2
Kevin <kevin_zino@yahoo.com>
2022-09-05 16:24:44 +0300

    Added 2 new packages - bcrypt and validator Introduced new custom static function(singup) in the user model Added email and password validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const e = require("express");
 
 // Login user
 const loginUser = async (req, res) => {
@@ -7,7 +8,15 @@ const loginUser = async (req, res) => {
 
 // Signup user
 const signupUser = async (req, res) => {
-    res.json({msg: "Signup user"})
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.signup(email, password);
+
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 module.exports = {signupUser, loginUser};
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +15,32 @@ const userSchema = new Schema({
         required: true,
     }
 });
+
+// Static signup method
+userSchema.statics.signup = async function (email, password) {
+    // Validation
+    if (!email || !password) {
+        throw Error("All fields must be filled.");
+    }
+
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid.");
+    }
+
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password not strong enough.");
+    }
+
+    const exists = await this.findOne({email});
+
+    if (exists) {
+        throw Error("Email already in use.");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    return await this.create({email, password: hash})
+}
 
 module.exports = mongoose.model("User", userSchema);
